4933505c67e69864328b07a23fc634e248cca4e8
Trina <trinamceary@gmail.com>
2020-04-30 12:48:58 -0700

    get key specific progression!

--- src/components/Jam/Jam.js
+++ src/components/Jam/Jam.js
@@ -4,6 +4,7 @@ import MusicProvider from '../../MusicContext';
 //import musicStore from '../../musicStore';
 import './Jam.css'
 
+    //each key and its major scale
     const majorScales = { 
       'A': ['A','B','Db','D','E','Gb','Ab','A'],
       'Ab': ['Ab','Bb','C','Db','Eb','F','G','Ab'],
@@ -19,6 +20,7 @@ import './Jam.css'
       'Gb': ['Gb','Ab','Bb','Cb','Db','Eb','F','Gb'],
     }
 
+    //each key and its minor scale
     const minorScales = { 
     'A': ['A','B','Db','D','E','Gb','Ab','A'],
     'Ab': ['Ab','Bb','C','Db','Eb','F','G','Ab'],
@@ -40,49 +42,72 @@ export default class Jam extends Component {
   displayProg() {
     const prog = this.context.prog
     if(prog.length < 1) {
-      return 'Please choose a progression'
+      return 'No progression was selected'
     }
     return prog.join('-')
   };
 
+  displayTonic() {
+    const tonic = this.context.tonic
+    if (tonic === '') {
+      return 'No tonic was selected'
+    }
+    return tonic
+  }
+
   displayKey() {
     const key = this.context.key
     if (key === '') {
-      return 'Please choose a key'
+      return 'No key was selected'
     }
     return key
+  };
+
+  getNoteIndexes(scale, prog) {
+    const result = []
+
+    for (let i = 0; i < prog.length; i++) {
+      const el = scale[prog[i]-1]
+
+      result.push(el)
+    }
+    return result
+  };
+
+  getKeySpecificProgression(key, tonic, prog) {
+    const keyTonic = tonic === 'major' ? majorScales : minorScales
+    const scale = keyTonic[key]
+
+    return this.getNoteIndexes(scale, prog)
   }
 
-  // renderChordButtons() {
-  //   const key = this.context.key
-  //   if (this.context.tonic === 'major') {
-  //     const prog = majorScales.key.[this.context.prog]
-  //   }
-
+  
+  // handleSetMelody() {
+  //   // const prog = this.context.prog
+  //   // const key = this.context.key
+  //   // const melody = [majorScales.key[prog]]
+  //   // console.log('melody', melody)
+  //   // this.context.setMelody(melody)
+  //   return
   // }
 
-  handleSetMelody() {
-    // const prog = this.context.prog
-    // const key = this.context.key
-    // const melody = [majorScales.key[prog]]
-    // console.log('melody', melody)
-    // this.context.setMelody(melody)
-    return
-  }
-
   render() {
-    //const melody = ['A', 'B', 'C']
+    const key = this.context.key
+    const tonic = this.context.tonic
+    const prog = this.context.prog
+
     return (
       <>
       <div className='jam-header'>
         <h1>Let's Jam!</h1>
-          <h2>Progression: {this.displayProg()}</h2>
-          <h2>Key: {this.displayKey()}</h2>
+        <h2>Key: {this.displayKey()}</h2>
+        <h2>Tonic: {this.displayTonic()}</h2>
+        <h2>Progression: {this.displayProg()}</h2>
+                 
       </div>
       <div className='chords'>
-        <button onClick={() => this.handleSetMelody()}>
-          Click to set melody
-        </button>
+        {console.log("getNoteIndexes(majorScales[C], [1,4,5]):",this.getNoteIndexes(majorScales["C"], [1,4,5]))}
+       <p>{this.getKeySpecificProgression(key ,tonic, prog)}</p> 
       </div>
 
       <div className='controls'>
